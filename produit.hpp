615b67bec19e329ead30d631eae0ced73e2ca94f
TanguyPellerin <37904230+TanguyPellerin@users.noreply.github.com>
2018-03-30 09:29:03 +0200

    Update produit.hpp

--- produit.hpp
+++ produit.hpp
@@ -1,14 +1,24 @@
-#define produit_hpp
-
 #include <stdio.h>
 #include <string>
+#include <iostream>
 
 using namespace std;
 
 
-
 class Produit
 {
+    
+    public :
+    
+    Produit(int prix, int quantite, int codebarre, string categorie, string detail, string nomProduit); //constructeur
+    
+    string p_nomProduit;
+    void affichage(); //permet d'afficher
+    void modif_prix(int newPrix); //permet de modifier la prix d'un produit
+    void modif_quantite(int Vquantite); //permet de modifier la quantité d'un produit
+    int codebarre(); //permet de récuper le codebarre d'un produit
+    
+    
     private :
     
     int p_prix;
@@ -16,13 +26,6 @@ class Produit
     int p_codebarre;
     string p_categorie;
     string p_detail;
-    string p_nomProduit;
     
-    
-    public :
-    
-    Produit(int prix, int quantite, int codebarre, string categorie, string detail, string nomProduit);
-    void affichage();
-    void modif_prix(int newPrix);
-    void modif_quantite(int Vquantite);
 };
+
