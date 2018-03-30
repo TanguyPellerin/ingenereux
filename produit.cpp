31898a01ea8d5d60877c0cbc32f49a11653f2a16
TanguyPellerin <37904230+TanguyPellerin@users.noreply.github.com>
2018-03-30 09:28:48 +0200

    Update produit.cpp

--- produit.cpp
+++ produit.cpp
@@ -1,54 +1,67 @@
 #include <iostream>
 #include <vector>
-
+#include <stdio.h>
 #include "produit.hpp"
+#include "caisse.hpp"
 
 using namespace std;
 
 
-void ajoutProduit(vector<Produit> tabproduit);
+void ajoutProduit(vector<Produit> *tabproduit);
 void afficherlisteproduit(vector<Produit> tabproduit);
+void supprimerproduit(vector<Produit> *tabproduit);
+int trouverelement(vector<Produit> tabproduit, string Psupprimer);
 
 
 int main()
 {
-    vector<Produit> tabproduit;
-    ajoutProduit(tabproduit);
-    afficherlisteproduit(tabproduit);
+    //vector<Produit> tabproduit, tablisteclient;
+    //Caisse caisse1;
+    //caisse1.pay(tabproduit, tablisteclient);
     
     
     return 0;
 }
 
 
-void ajoutProduit(vector<Produit> tabproduit)
+void ajoutProduit(vector<Produit> *tabproduit)
 {
     int prix, quantite, codebarre;
     string categorie, detail, nomProduit;
-    cout << "Quel est le nom du produit : " << endl;
+    cout << "Quel est le nom du produit : ";
     cin >> nomProduit;
-    cout << "Quel est le prix du produit : " << endl;
+    cout << "Quel est le prix du produit : ";
     cin >> prix;
-    cout << "Quelle est la quantité du produit : " << endl;
+    cout << "Quelle est la quantité du produit : ";
     cin >> quantite;
-    cout << "Quelle est le codebarre du produit : " << endl;
+    cout << "Quelle est le codebarre du produit : ";
     cin >> codebarre;
-    cout << "Quelle est la catégorie du produit : " << endl;
+    cout << "Quelle est la catégorie du produit : ";
     cin >> categorie;
-    cout << "quel est le detail du produit : " << endl;
+    cout << "quel est le detail du produit : ";
     cin >> detail;
     
     Produit newproduit(prix, quantite, codebarre, categorie, detail, nomProduit);
-    tabproduit.push_back(newproduit);
+    tabproduit->push_back(newproduit);
 
 }
+
 
 void afficherlisteproduit(vector<Produit> tabproduit)
 {
     for (int i=0; i<tabproduit.size(); i++)
     {
         tabproduit[i].affichage();
-        cout << "yo";
     }
     
+}
+
+
+void supprimerproduit(vector<Produit> *tabproduit)
+{
+    string Psupprimer;
+    cout << "Quel élément voulez vous supprimer ? : ";
+    cin >> Psupprimer;
+    int position = trouverelement(*tabproduit, Psupprimer);
+    *tabproduit->erase(tabproduit->begin()+position);
 }
