9debac87b8cc5efe64116ab1ed4e6aa3b74a6859
TanguyPellerin <37904230+TanguyPellerin@users.noreply.github.com>
2018-03-30 10:16:05 +0200

    Update caisse.hpp

--- caisse.hpp
+++ caisse.hpp
@@ -1,45 +1,88 @@
+#ifndef DEF_CAISSE
+#define DEF_CAISSE
+
+
+#include "caisse.hpp"
 #include "produit.hpp"
 #include <string.h>
 #include <stdio.h>
+#include <vector>
 #include <iostream>
-
 
 using namespace std;
 
-void Produit::modif_prix(int newPrix)
+
+void afficherlisteproduit(vector<Produit> tabproduit)
 {
-    p_prix = newPrix;
-}
-
-
-
-void Produit::modif_quantite(int Vquantite)
-{
-    p_quantite -= Vquantite;
-    if (p_quantite<0)
+    for (int i=0; i<tabproduit.size(); i++)
     {
-        p_quantite=0;
+        tabproduit[i].affichage();
     }
-}
-
-
-void Produit::affichage()
-{
-    cout << "Nom produit : " << p_nomProduit << endl;
-    cout << "Prix : " << p_prix << "€" << endl;
-    cout << "Quantite : " << p_quantite << endl;
-    cout << "Codebarre : " << p_codebarre <<endl;
-    cout << "Catégorie : " << p_categorie <<endl;
-}
-
-int Produit::codebarre()
-{
-    return p_codebarre;
-}
-
-
-Produit::Produit(int prix, int quantite, int codebarre, string categorie, string detail, string nomProduit) : p_prix(prix), p_quantite(quantite), p_codebarre(codebarre), p_categorie(categorie), p_detail(detail), p_nomProduit(nomProduit)
-{
     
 }
+
+
+void Caisse::pay(vector<Produit> tabproduit, vector<Produit> tablisteclient)
+{
+    int nbrArticle;
+    cout << "Combien d'article avez vous a scanner : ";
+    cin >> nbrArticle;
+    for (int i=0; i<nbrArticle; i++)
+    {
+        Caisse:scan(tabproduit, &tablisteclient);
+    }
+    afficherlisteproduit(tablisteclient);
+    
+    
+}
+
+
+int trouverElementCodebarre(vector<Produit> tabproduit, int element)
+{
+    
+    int i=0;
+    for (i=0; i<tabproduit.size(); i++)
+    {
+        if (tabproduit[i].codebarre()==element)
+        {
+            break;
+        }
+    }
+    
+    return i;
+}
+
+
+int trouverElementNom(vector<Produit> tabproduit, string element)
+{
+    
+    int i=0;
+    for (i=0; i<tabproduit.size(); i++)
+    {
+        if (tabproduit[i].p_nomProduit==element)
+        {
+            break;
+        }
+    }
+    
+    return i;
+}
+
+
+void Caisse::scan(vector<Produit> tabproduit, vector<Produit> *tablisteclient)
+{
+    int codebarre, nbrarticle;
+    cout << "Rentrer le codebarre du produit : ";
+    cin >> codebarre;
+    
+    int position = trouverElementCodebarre(tabproduit, codebarre);
+    cout << "Combien de fois il y a cette article : ";
+    cin >> nbrarticle;
+    tablisteclient->push_back(tabproduit[position]);
+}
+
+
+#endif
+
+
 
